b06aff943a103fb70fea52ac5a1b4874605883cd
VENKATESHCODER <122370232+VENKATESHCODER@users.noreply.github.com>
2023-03-10 23:50:36 +0530

    Expenseitem.js created

---
+++ Expenseitem.js
@@ -0,0 +1,22 @@
+function Expenseitem() {
+    return <div className="App">
+        <header className="App-header">
+        <div>Expenseitem</div>
+        <div>
+            <h2>Food</h2>
+            <div>Rs.10</div>
+        </div>
+        <div>
+            <h2>Petrol</h2>
+            <div>Rs.100</div>
+        </div>
+        <div>
+            <h2>Movies</h2>
+            <div>Rs.200</div>
+        </div>
+        </header>
+    </div>
+    
+}
+
+export default Expenseitem;     
