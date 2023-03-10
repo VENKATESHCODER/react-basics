39a16b5cfb6d533a09df78f8233baf0478172684
VENKATESHCODER <122370232+VENKATESHCODER@users.noreply.github.com>
2023-03-10 23:47:22 +0530

    app.js created

---
+++ app.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Expenseitem from './Expenseitem';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <h5>Lets get started
+        </h5>
+        <h2>Expenseitem!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
